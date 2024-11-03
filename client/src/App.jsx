dbd02d824a54348d863fe6ef7a2dc62243179a61
yeyuxuan <yyxtheoracle@126.com>
2024-11-03 22:18:38 +0800

    dependent queries

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import PostsList1 from "./PostsList1";
 import PostsList2 from "./PostsList2";
+import Post from "./Post";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState(<PostsList1 />);
@@ -13,6 +14,10 @@ export default function App() {
 
       <button onClick={() => setCurrentPage(<PostsList2 />)}>
         Posts List 2
+      </button>
+
+      <button onClick={() => setCurrentPage(<Post id={1} />)}>
+        First Post
       </button>
 
       {currentPage}
--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,22 +1,22 @@
-import { useQuery } from "@tanstack/react-query"
-import { getPost } from "./api/posts"
-import { getUser } from "./api/users"
+import { useQuery } from "@tanstack/react-query";
+import { getPost } from "./api/posts";
+import { getUser } from "./api/users";
 
 export default function Post({ id }) {
   const postQuery = useQuery({
     queryKey: ["posts", id],
     queryFn: () => getPost(id),
-  })
+  });
 
-  const userQuery = useQuery({
-    queryKey: ["users", postQuery?.data?.userId],
-    enabled: postQuery?.data?.userId != null,
-    queryFn: () => getUser(postQuery.data.userId),
-  })
+  // const userQuery = useQuery({
+  //   queryKey: ["users", postQuery?.data?.userId],
+  //   enabled: postQuery?.data?.userId != null,
+  //   queryFn: () => getUser(postQuery.data.userId),
+  // });
 
-  if (postQuery.status === "loading") return <h1>Loading...</h1>
+  if (postQuery.status === "loading") return <h1>Loading...</h1>;
   if (postQuery.status === "error") {
-    return <h1>{JSON.stringify(postQuery.error)}</h1>
+    return <h1>{JSON.stringify(postQuery.error)}</h1>;
   }
 
   return (
@@ -24,14 +24,17 @@ export default function Post({ id }) {
       <h1>
         {postQuery.data.title} <br />
         <small>
-          {userQuery.isLoading
+          {/* {postQuery.data.userId} */}
+
+          {/* {userQuery.isLoading
             ? "Loading User..."
             : userQuery.isError
             ? "Error Loading User"
-            : userQuery.data.name}
+            : userQuery.data.name} */}
         </small>
       </h1>
+
       <p>{postQuery.data.body}</p>
     </>
-  )
+  );
 }
